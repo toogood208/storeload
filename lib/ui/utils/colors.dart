046a9c56de3e8eefff3d51daeb3d1c953e0a7239
Sigurd <82845772+toogood208@users.noreply.github.com>
2023-03-07 21:42:17 +0100

    implement splashscreen view

---
+++ lib/ui/utils/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color kBackgroundColor = const Color(0XFF001B2E);
+Color kWhiteColor = const Color(0XFFFFFFFF);
---
+++ lib/ui/utils/test_styles.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:storeload/core/extensions/to_figma_height.dart';
+import 'package:storeload/ui/utils/colors.dart';
+
+TextStyle kClashGrotesk128Medium = TextStyle(
+  fontFamily: "ClashGrotesk",
+  fontWeight: FontWeight.w500,
+  fontSize: 128.sp,
+  height: 128.sp.toFigmaHeight(192)
+);
+
+TextStyle kClashGrotesk88Medium = TextStyle(
+  fontFamily: "ClashGrotesk",
+  fontWeight: FontWeight.w500,
+  fontSize: 88.sp,
+  height: 88.sp.toFigmaHeight(192)
+);
+
+TextStyle kClashGrotesk64Medium = TextStyle(
+  fontFamily: "ClashGrotesk",
+  fontWeight: FontWeight.w500,
+  fontSize: 64.sp,
+  height: 64.sp.toFigmaHeight(96)
+);
+
+TextStyle kClashGrotesk48Medium = TextStyle(
+  fontFamily: "ClashGrotesk",
+  fontWeight: FontWeight.w500,
+  fontSize: 48.sp,
+  height: 48.sp.toFigmaHeight(72)
+);
+
+TextStyle kClashGrotesk40Medium = TextStyle(
+  fontFamily: "ClashGrotesk",
+  fontWeight: FontWeight.w500,
+  fontSize: 40.sp,
+  height: 40.sp.toFigmaHeight(60)
+);
+
+TextStyle kClashGrotesk32Medium = TextStyle(
+  fontFamily: "ClashGrotesk",
+  fontWeight: FontWeight.w500,
+  fontSize: 32.sp,
+  height: 32.sp.toFigmaHeight(48),
+  color: kWhiteColor,
+);
+
+TextStyle kClashGrotesk24Medium = TextStyle(
+  fontFamily: "ClashGrotesk",
+  fontWeight: FontWeight.w500,
+  fontSize: 24.sp,
+  height: 24.sp.toFigmaHeight(36)
+);
+
+TextStyle kClashGrotesk20Medium = TextStyle(
+  fontFamily: "ClashGrotesk",
+  fontWeight: FontWeight.w500,
+  fontSize: 20.sp,
+  height: 20.sp.toFigmaHeight(30)
+);
+
+TextStyle kClashGrotesk18Medium = TextStyle(
+  fontFamily: "ClashGrotesk",
+  fontWeight: FontWeight.w500,
+  fontSize: 18.sp,
+  height: 18.sp.toFigmaHeight(27)
+);
+
+TextStyle kClashGrotesk16Medium = TextStyle(
+  fontFamily: "ClashGrotesk",
+  fontWeight: FontWeight.w500,
+  fontSize: 16.sp,
+  height: 16.sp.toFigmaHeight(24)
+);
+
+TextStyle kClashGrotesk14Medium = TextStyle(
+  fontFamily: "ClashGrotesk",
+  fontWeight: FontWeight.w500,
+  fontSize: 14.sp,
+  height: 14.sp.toFigmaHeight(20)
+);
+
+TextStyle kClashGrotesk12Medium = TextStyle(
+  fontFamily: "ClashGrotesk",
+  fontWeight: FontWeight.w500,
+  fontSize: 12.sp,
+  height: 12.sp.toFigmaHeight(18)
+);
