2045d427a4d183b9db3688a56d538cb37b5f3c26
Sigurd <82845772+toogood208@users.noreply.github.com>
2023-03-18 01:20:48 +0100

    refactor

--- lib/core/services/network_services/network_constant.dart
+++ lib/core/services/network_services/network_constant.dart
@@ -1,3 +1,4 @@
 const baseUrl = "https://store-reload.onrender.com/api/v1";
 const createUserEndpoint = "/auth/create";
 const signInUserEndpoint = "/auth/login";
+const forgotPassswordEndpoint = "/user/email-verify";
